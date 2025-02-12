6beb5ed7a02d32c5556de80f12dfe4c2c9a501bf
Homrajshahi <bindashahi473@gmail.com>
2025-02-12 23:33:18 +0545

    making a traffic_generator.h

---
+++ include/traffic_generator.h
@@ -0,0 +1,18 @@
+#ifndef TRAFFIC_GENERATOR_H
+#define TRAFFIC_GENERATOR_H
+
+#include "queue.h"
+
+// Window and road dimensions
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define ROAD_WIDTH 120
+#define LANE_WIDTH (ROAD_WIDTH/3)
+#define VEHICLE_SIZE 20
+
+void generateVehicle(const char* filename);
+void startVehicleGeneration(const char* filename);
+void writeVehicleToFile(Vehicle* vehicle, const char* filename);
+
+#endif
+
