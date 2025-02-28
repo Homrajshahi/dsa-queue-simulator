310e9b3fc80bf7b865825ff9d50c4f54ab183809
Homrajshahi <bindashahi473@gmail.com>
2025-02-28 09:56:36 +0545

    successfully completed the deque problem

--- src/queue.c
+++ src/queue.c
@@ -1,11 +1,6 @@
 #include <stdlib.h>
-<<<<<<< HEAD
-#include "queue.h"
-#include "vehicle.h"
-=======
 #include <math.h>
 #include "queue.h"
->>>>>>> b73988b (solving a problem of dequw)
 
 Queue* createQueue(Direction direction, LanePosition lane) {
     Queue* queue = (Queue*)malloc(sizeof(Queue));
@@ -17,22 +12,13 @@ Queue* createQueue(Direction direction, LanePosition lane) {
     queue->direction = direction;
     queue->lane = lane;
     queue->waitingTime = 0;
-<<<<<<< HEAD
 
-    // Set priority lane flags based on lane and direction
     queue->isPriorityLane = (lane == LANE_CENTER) &&
-                           (direction == DIRECTION_NORTH || // AL2
-                            direction == DIRECTION_SOUTH || // CL2
-                            direction == DIRECTION_EAST ||  // BL2
-                            direction == DIRECTION_WEST);   // DL2
+                           (direction == DIRECTION_SOUTH || // AL2
+                            direction == DIRECTION_WEST ||  // BL2
+                            direction == DIRECTION_EAST ||  // CL2
+                            direction == DIRECTION_NORTH);  // DL2
 
-    // Initialize all items to NULL
-=======
-    queue->isPriorityLane = (lane == LANE_CENTER) &&
-                           (direction == DIRECTION_NORTH || direction == DIRECTION_SOUTH ||
-                            direction == DIRECTION_EAST || direction == DIRECTION_WEST);
-
->>>>>>> b73988b (solving a problem of dequw)
     for (int i = 0; i < MAX_QUEUE_SIZE; i++) {
         queue->items[i] = NULL;
     }
@@ -53,24 +39,13 @@ void enqueue(Queue* queue, Vehicle* vehicle) {
     queue->rear = (queue->rear + 1) % MAX_QUEUE_SIZE;
     queue->items[queue->rear] = vehicle;
     queue->size++;
-<<<<<<< HEAD
 
-    // Update vehicle's priority lane status
-    vehicle->isPriorityLane = queue->isPriorityLane;
-
-    // Adjust waiting time based on vehicle type and lane
-    if (vehicle->type > 0) { // Emergency vehicles
-        queue->waitingTime += 2;
-    } else if (queue->isPriorityLane && queue->size > 5) {
-        queue->waitingTime += 1.5; // Priority lanes with high congestion
-=======
     vehicle->isPriorityLane = queue->isPriorityLane;
 
     if (vehicle->type > 0) {
         queue->waitingTime += 2;
     } else if (queue->isPriorityLane && queue->size > 5) {
         queue->waitingTime += 1.5;
->>>>>>> b73988b (solving a problem of dequw)
     } else {
         queue->waitingTime += 1;
     }
@@ -84,10 +59,6 @@ Vehicle* dequeue(Queue* queue) {
     queue->front = (queue->front + 1) % MAX_QUEUE_SIZE;
     queue->size--;
 
-<<<<<<< HEAD
-    // Update waiting time based on vehicle type
-=======
->>>>>>> b73988b (solving a problem of dequw)
     if (vehicle) {
         if (vehicle->type > 0) {
             queue->waitingTime = fmax(0, queue->waitingTime - 2);
@@ -97,10 +68,7 @@ Vehicle* dequeue(Queue* queue) {
             queue->waitingTime = fmax(0, queue->waitingTime - 1);
         }
     }
-<<<<<<< HEAD
 
-=======
->>>>>>> b73988b (solving a problem of dequw)
     return vehicle;
 }
 
@@ -112,8 +80,6 @@ Vehicle* peekFront(Queue* queue) {
     if (!queue || isEmpty(queue)) return NULL;
     return queue->items[queue->front];
 }
-<<<<<<< HEAD
-=======
 
 bool isEmergencyVehicle(Vehicle* vehicle) {
     return vehicle && vehicle->type > 0;
@@ -129,60 +95,55 @@ void setVehiclePath(Vehicle* vehicle) {
     vehicle->endDirection = vehicle->startDirection;
     vehicle->endLane = vehicle->startLane;
     vehicle->turning = false;
+    vehicle->passedIntersection = false;
 
-    // Left lane: Always turn left to the right lane of the next road
-    if (vehicle->startLane == LANE_LEFT) {
+    if (vehicle->startLane == LANE_LEFT) { // L1 turns left
         switch (vehicle->startDirection) {
-            case DIRECTION_SOUTH: // Road A (South) -> Road B (West)
-                vehicle->endDirection = DIRECTION_WEST;
-                vehicle->endLane = LANE_RIGHT;
-                break;
-            case DIRECTION_EAST: // Road C (East) -> Road A (South)
-                vehicle->endDirection = DIRECTION_SOUTH;
-                vehicle->endLane = LANE_RIGHT;
-                break;
-            case DIRECTION_NORTH: // Road D (North) -> Road C (East)
-                vehicle->endDirection = DIRECTION_EAST;
-                vehicle->endLane = LANE_RIGHT;
-                break;
-            case DIRECTION_WEST: // Road B (West) -> Road D (North)
-                vehicle->endDirection = DIRECTION_NORTH;
-                vehicle->endLane = LANE_RIGHT;
-                break;
+            case DIRECTION_SOUTH: vehicle->endDirection = DIRECTION_WEST; break;  // A -> B (BL3)
+            case DIRECTION_WEST:  vehicle->endDirection = DIRECTION_SOUTH; break; // B -> A (AL3)
+            case DIRECTION_EAST:  vehicle->endDirection = DIRECTION_NORTH; break; // C -> D (DL3)
+            case DIRECTION_NORTH: vehicle->endDirection = DIRECTION_EAST; break;  // D -> C (CL3)
         }
-        vehicle->turning = true; // Mark for turning, handled by simulator
+        vehicle->turning = true;
+        vehicle->endLane = LANE_RIGHT;
     }
-    // Center lane: Randomly turn left or right to the right lane of the next road
-    else if (vehicle->startLane == LANE_CENTER) {
+    else if (vehicle->startLane == LANE_CENTER) { // L2 turns left or right with restrictions
         int turnChoice = rand() % 2; // 0 = left, 1 = right
         switch (vehicle->startDirection) {
-            case DIRECTION_SOUTH: // Road A -> Road D (North) or Road C (East)
-                vehicle->endDirection = turnChoice ? DIRECTION_EAST : DIRECTION_NORTH;
-                vehicle->endLane = LANE_RIGHT;
+            case DIRECTION_SOUTH: // A
+                vehicle->endDirection = turnChoice ? DIRECTION_EAST : DIRECTION_NORTH; // CL3 or DL3
                 break;
-            case DIRECTION_EAST: // Road C -> Road B (West) or Road D (North)
-                vehicle->endDirection = turnChoice ? DIRECTION_WEST : DIRECTION_NORTH;
-                vehicle->endLane = LANE_RIGHT;
+            case DIRECTION_WEST:  // B
+                vehicle->endDirection = turnChoice ? DIRECTION_EAST : DIRECTION_SOUTH; // CL3 or AL3
                 break;
-            case DIRECTION_NORTH: // Road D -> Road B (West) or Road A (South)
-                vehicle->endDirection = turnChoice ? DIRECTION_WEST : DIRECTION_SOUTH;
-                vehicle->endLane = LANE_RIGHT;
+            case DIRECTION_EAST:  // C
+                vehicle->endDirection = turnChoice ? DIRECTION_NORTH : DIRECTION_WEST; // DL3 or BL3
                 break;
-            case DIRECTION_WEST: // Road B -> Road C (East) or Road A (South)
-                vehicle->endDirection = turnChoice ? DIRECTION_NORTH : DIRECTION_SOUTH;
-                vehicle->endLane = LANE_RIGHT;
+            case DIRECTION_NORTH: // D
+                vehicle->endDirection = turnChoice ? DIRECTION_SOUTH : DIRECTION_WEST; // AL3 or BL3
                 break;
         }
-        vehicle->turning = true; // Mark for turning, handled by simulator
+        vehicle->turning = true;
+        vehicle->endLane = LANE_RIGHT;
     }
-    // Right lane: No straight movement, used for incoming traffic
 }
 
 bool isSafeDistance(Vehicle* v1, Vehicle* v2) {
     if (!v1 || !v2) return true;
 
-    float distance = sqrt(pow(v2->x - v1->x, 2) + pow(v2->y - v1->y, 2));
-    float requiredDistance = SAFE_DISTANCE * (v1->type > 0 || v2->type > 0 ? 1.5 : 1.0);
+    float v1CenterX = v1->x;
+    float v1CenterY = v1->y;
+    float v2CenterX = v2->x;
+    float v2CenterY = v2->y;
+
+    float distance = sqrt(pow(v2CenterX - v1CenterX, 2) +
+                         pow(v2CenterY - v1CenterY, 2));
+
+    float requiredDistance = SAFE_DISTANCE;
+    if (v1->type > 0 || v2->type > 0) {
+        requiredDistance *= 1.5;
+    }
+
     return distance >= requiredDistance;
 }
 
@@ -199,49 +160,97 @@ bool checkCollision(Vehicle* v1, Vehicle* v2) {
     float v2Top = v2->y - SAFE_DISTANCE/4;
     float v2Bottom = v2->y + SAFE_DISTANCE/4;
 
-    return !(v1Right < v2Left || v1Left > v2Right || v1Bottom < v2Top || v1Top > v2Bottom);
+    return !(v1Right < v2Left || v1Left > v2Right ||
+             v1Bottom < v2Top || v1Top > v2Bottom);
 }
 
 int getQueueSize(Direction direction, LanePosition lane) {
-    return (lane == LANE_CENTER && (direction == DIRECTION_NORTH || direction == DIRECTION_SOUTH ||
-                                    direction == DIRECTION_EAST || direction == DIRECTION_WEST)) ? 6 : 0;
+    if (lane == LANE_CENTER &&
+        (direction == DIRECTION_SOUTH || // AL2
+         direction == DIRECTION_WEST ||  // BL2
+         direction == DIRECTION_EAST ||  // CL2
+         direction == DIRECTION_NORTH)) { // DL2
+        return 6;
+    }
+    return 0;
 }
 
 bool canProceedThroughIntersection(Vehicle* vehicle, bool trafficLights[4]) {
     if (!vehicle) return false;
 
+    if (vehicle->passedIntersection) return true;
+
     bool canProceed = false;
+
     switch (vehicle->startDirection) {
-        case DIRECTION_SOUTH: canProceed = trafficLights[1]; break; // t2
-        case DIRECTION_EAST:  canProceed = trafficLights[0]; break; // t1
-        case DIRECTION_NORTH: canProceed = trafficLights[3]; break; // t4
-        case DIRECTION_WEST:  canProceed = trafficLights[2]; break; // t3
+        case DIRECTION_SOUTH:  // Road A - t2
+            if (vehicle->startLane == LANE_LEFT) { // AL1 -> BL3
+                canProceed = trafficLights[1]; // t2
+            } else if (vehicle->startLane == LANE_CENTER) { // AL2 -> DL3 or CL3
+                canProceed = trafficLights[1]; // t2
+            } else { // AL3 <- CL1 (t1), BL2 (t3), DL2 (t4)
+                canProceed = trafficLights[1]; // t2
+            }
+            break;
+
+        case DIRECTION_WEST:  // Road B - t3
+            if (vehicle->startLane == LANE_LEFT) { // BL1 -> AL3
+                canProceed = trafficLights[2]; // t3
+            } else if (vehicle->startLane == LANE_CENTER) { // BL2 -> AL3 or CL3
+                canProceed = trafficLights[2]; // t3
+            } else { // BL3 <- AL1 (t2), CL2 (t1), DL2 (t4)
+                canProceed = trafficLights[2]; // t3
+            }
+            break;
+
+        case DIRECTION_EAST:  // Road C - t1
+            if (vehicle->startLane == LANE_LEFT) { // CL1 -> DL3
+                canProceed = trafficLights[0]; // t1
+            } else if (vehicle->startLane == LANE_CENTER) { // CL2 -> BL3 or DL3
+                canProceed = trafficLights[0]; // t1
+            } else { // CL3 <- AL2 (t2), BL2 (t3), DL1 (t4)
+                canProceed = trafficLights[0]; // t1
+            }
+            break;
+
+        case DIRECTION_NORTH:  // Road D - t4
+            if (vehicle->startLane == LANE_LEFT) { // DL1 -> CL3
+                canProceed = trafficLights[3]; // t4
+            } else if (vehicle->startLane == LANE_CENTER) { // DL2 -> BL3 or AL3
+                canProceed = trafficLights[3]; // t4
+            } else { // DL3 <- BL1 (t3), CL2 (t1), AL2 (t2)
+                canProceed = trafficLights[3]; // t4
+            }
+            break;
     }
 
-    if (vehicle->isPriorityLane && getQueueSize(vehicle->startDirection, LANE_CENTER) > 5) return true;
-    if (isEmergencyVehicle(vehicle)) return true;
+    if (vehicle->isPriorityLane && getQueueSize(vehicle->startDirection, LANE_CENTER) > 5) {
+        return true;
+    }
+
+    if (isEmergencyVehicle(vehicle)) {
+        return true;
+    }
+
     return canProceed;
 }
 
 void updateVehiclePosition(Vehicle* vehicle) {
     if (!vehicle) return;
 
-    if (!vehicle->hasPassed) {
-        // Straight movement until turning or passing
-        switch (vehicle->startDirection) {
-            case DIRECTION_NORTH: vehicle->y -= vehicle->speed; break;
-            case DIRECTION_EAST:  vehicle->x += vehicle->speed; break;
-            case DIRECTION_SOUTH: vehicle->y += vehicle->speed; break;
-            case DIRECTION_WEST:  vehicle->x -= vehicle->speed; break;
+    if (vehicle->turning && vehicle->progress < 1.0f) {
+        vehicle->progress += vehicle->speed / 100.0f;
+        if (vehicle->progress >= 1.0f) {
+            vehicle->turning = false;
+            vehicle->progress = 1.0f;
+            vehicle->passedIntersection = true;
         }
     } else {
-        // Continue straight after passing
         switch (vehicle->startDirection) {
-            case DIRECTION_NORTH: vehicle->y -= vehicle->speed; break;
-            case DIRECTION_EAST:  vehicle->x += vehicle->speed; break;
-            case DIRECTION_SOUTH: vehicle->y += vehicle->speed; break;
-            case DIRECTION_WEST:  vehicle->x -= vehicle->speed; break;
+            case DIRECTION_SOUTH: vehicle->y -= vehicle->speed; break; // A
+            case DIRECTION_WEST:  vehicle->x -= vehicle->speed; break; // B
+            case DIRECTION_EAST:  vehicle->x += vehicle->speed; break; // C
+            case DIRECTION_NORTH: vehicle->y += vehicle->speed; break; // D
         }
     }
 }
->>>>>>> b73988b (solving a problem of dequw)
